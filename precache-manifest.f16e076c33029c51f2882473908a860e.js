9ce88d6bceff9d82a0be99166641a5a3369c0718
Marc-Olivier <wormsparty@gmail.com>
2019-09-12 21:23:26 +0200

    Switch to React version

---
+++ precache-manifest.f16e076c33029c51f2882473908a860e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6964c71c33a968e76894dba7f40df789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1742d708fdf54df8541",
+    "url": "/static/css/main.24a6efe5.chunk.css"
+  },
+  {
+    "revision": "432e5fc72e93133ca0a2",
+    "url": "/static/js/2.a108e76d.chunk.js"
+  },
+  {
+    "revision": "a1742d708fdf54df8541",
+    "url": "/static/js/main.0b2b2a7d.chunk.js"
+  },
+  {
+    "revision": "6a193a9c29f7a6fbcfb4",
+    "url": "/static/js/runtime~main.fc6c27ef.js"
+  }
+]);
